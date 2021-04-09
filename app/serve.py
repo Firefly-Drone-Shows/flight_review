e14170a85153a2e84026d185a62efbaa9b71ee8c
Tyler Limkemann <tyler@fireflydroneshows.com>
2021-04-09 16:25:06 -0700

    Fix bulk upload to ignore folders and non-ULog files

--- app/serve.py
+++ app/serve.py
@@ -145,31 +145,33 @@ if args.bulkupload:
     if os.path.isdir(folder_path):
         con = sqlite3.connect(get_db_filename())
         cur = con.cursor()
-        for filename in os.listdir(folder_path):
-            with open(os.path.join(folder_path, filename), 'r') as file:
-                # TODO: do actual validation here, don't just check filename
-                _, ext = os.path.splitext(filename)
-                if ext not in ['.ulg', '.ulog']:
-                    print(f'Skipping non-ULog file {folder_path}/{filename}')
-                    continue
-                # TODO: PLEASE don't do this, make save_uploaded_log work with real file-like objects
-                file.move = types.MethodType(_move_file_monkeypatch, file) 
-                file.get_filename = types.MethodType(lambda self: filename, file)
-                formdict = {}
-                formdict['description'] = ''
-                formdict['email'] = ''
-                formdict['upload_type'] = 'personal'
-                formdict['source'] = 'bulk'
-                formdict['title'] = ''
-                formdict['obfuscated'] = 0
-                formdict['allow_for_analysis'] = 1
-                formdict['feedback'] = ''
-                formdict['wind_speed'] = -1
-                formdict['rating'] = ''
-                formdict['video_url'] = ''
-                formdict['is_public'] = 1
-                formdict['vehicle_name'] = ''
-                formdict['error_labels'] = ''
+        for root, dirs, files in os.walk(folder_path):
+            for file_name in files:
+                file_path = os.path.join(root, file_name)
+                with open(file_path, 'r') as file:
+                    # TODO: do actual validation here, don't just check filename
+                    _, ext = os.path.splitext(file_name)
+                    if ext not in ['.ulg', '.ulog']:
+                        print(f'Skipping non-ULog file {file_path}')
+                        continue
+                    # TODO: PLEASE don't do this, make save_uploaded_log work with real file-like objects
+                    file.move = types.MethodType(_move_file_monkeypatch, file) 
+                    file.get_filename = types.MethodType(lambda self: filename, file)
+                    formdict = {}
+                    formdict['description'] = ''
+                    formdict['email'] = ''
+                    formdict['upload_type'] = 'personal'
+                    formdict['source'] = 'bulk'
+                    formdict['title'] = ''
+                    formdict['obfuscated'] = 0
+                    formdict['allow_for_analysis'] = 1
+                    formdict['feedback'] = ''
+                    formdict['wind_speed'] = -1
+                    formdict['rating'] = ''
+                    formdict['video_url'] = ''
+                    formdict['is_public'] = 1
+                    formdict['vehicle_name'] = ''
+                    formdict['error_labels'] = ''
 
                 log_id = save_uploaded_log(con, cur, file, formdict)
                 print('/plot_app?log='+log_id)
